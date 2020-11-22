813b42cfa932ba3f9580fdf975138d933ab3dbcb
bunkerity <contact@bunkerity.com>
2020-11-22 17:38:07 +0100

    php and nextcloud examples fix

--- entrypoint/site-config.sh
+++ entrypoint/site-config.sh
@@ -128,15 +128,15 @@ fi
 # remote PHP
 if [ "$REMOTE_PHP" != "" ] ; then
 	replace_in_file "${NGINX_PREFIX}server.conf" "%USE_PHP%" "include ${NGINX_PREFIX}php.conf;"
+	replace_in_file "${NGINX_PREFIX}server.conf" "%FASTCGI_PATH%" "include ${NGINX_PREFIX}fastcgi.conf;"
 	replace_in_file "${NGINX_PREFIX}php.conf" "%REMOTE_PHP%" "$REMOTE_PHP"
-	replace_in_file "${NGINX_PREFIX}php.conf" "%FASTCGI_PATH%" "$NGINX_PREFIX"
 	if [ "$MULTISITE" = "yes" ] ; then
 		cp /etc/nginx/fastcgi.conf ${NGINX_PREFIX}fastcgi.conf
-		cp /etc/nginx/fastcgi_params ${NGINX_PREFIX}fastcgi_params
 	fi
 	replace_in_file "${NGINX_PREFIX}fastcgi.conf" "\$document_root" "${REMOTE_PHP_PATH}/"
 else
 	replace_in_file "${NGINX_PREFIX}server.conf" "%USE_PHP%" ""
+	replace_in_file "${NGINX_PREFIX}server.conf" "%FASTCGI_PATH%" ""
 fi
 
 # serve files
