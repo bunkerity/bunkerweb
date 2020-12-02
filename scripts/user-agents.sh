2fe05d3fd3d6a76989e5ecbf4002bd3e0d3872d3
bunkerity <contact@bunkerity.com>
2020-12-02 14:31:54 +0100

    fixing scripts again and again

--- scripts/user-agents.sh
+++ scripts/user-agents.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
-# replace pattern in file
-function replace_in_file() {
-        # escape slashes
-        pattern=$(echo "$2" | sed "s/\//\\\\\//g")
-        replace=$(echo "$3" | sed "s/\//\\\\\//g")
-        replace=$(echo "$replace" | sed "s/\\ /\\\\ /g")
-        sed -i "s/$pattern/$replace/g" "$1"
-}
+if [ ! -f "/etc/nginx/map-user-agent.conf" ] ; then
+	echo "" > /etc/nginx/map-user-agent.conf
+fi
 
 BLACKLIST="$(curl -s https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/_generator_lists/bad-user-agents.list)"
 DATA=""
 IFS=$'\n'
 for ua in $BLACKLIST ; do
-        DATA="${DATA}~*(?:\\\\b)${ua}\(?:\\\\b) yes;\n"
+        DATA="${DATA}~*(?:\\\\b)${ua}(?:\\\\b) yes;\n"
 done
 DATA_ESCAPED=$(echo "$DATA" | sed 's: :\\\\ :g' | sed 's:\\\\ yes;: yes;:g' | sed 's:\\\\\\ :\\\\ :g')
 
-cp /opt/confs/global/map-user-agent.conf /etc/nginx/map-user-agent.conf
-replace_in_file "/etc/nginx/map-user-agent.conf" "%BLOCK_USER_AGENT%" "$DATA_ESCAPED"
+echo -e "map \$http_user_agent \$bad_user_agent { default no; $DATA_ESCAPED }" > /etc/nginx/map-user-agent.conf
 cp /etc/nginx/map-user-agent.conf /cache
 
 if [ -f /tmp/nginx.pid ] ; then
