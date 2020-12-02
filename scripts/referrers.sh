ed8bd902b1b9eb86cb2e4e9d4882fbf9ad72c811
bunkerity <contact@bunkerity.com>
2020-12-02 11:09:38 +0100

    fix referrers script

--- scripts/referrers.sh
+++ scripts/referrers.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# replace pattern in file
-function replace_in_file() {
-        # escape slashes
-        pattern=$(echo "$2" | sed "s/\//\\\\\//g")
-        replace=$(echo "$3" | sed "s/\//\\\\\//g")
-        replace=$(echo "$replace" | sed "s/\\ /\\\\ /g")
-        sed -i "s/$pattern/$replace/g" "$1"
-}
-
 BLACKLIST="$(curl -s https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/_generator_lists/bad-referrers.list)"
 DATA=""
 IFS=$'\n'
@@ -16,8 +7,7 @@ for ref in $BLACKLIST ; do
         DATA="${DATA}\"~${ref}\" yes;\n"
 done
 
-cp /opt/confs/global/map-referrer.conf /etc/nginx/map-referrer.conf
-replace_in_file "/etc/nginx/map-referrer.conf" "%BLOCK_REFERRER%" "$DATA"
+echo "map \$http_referer \$bad_referrer { hostnames; default no; $DATA }" > /etc/nginx/map-referrer.conf
 cp /etc/nginx/map-referrer.conf /cache
 
 if [ -f /tmp/nginx.pid ] ; then
