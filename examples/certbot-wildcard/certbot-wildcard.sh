a3cfb50b4d7d17b9f2affe641f874a16f5e07b6c
florian <contact@bunkerity.com>
2021-04-26 21:34:18 +0200

    example - fix certbot wildcard

--- examples/certbot-wildcard/certbot-wildcard.sh
+++ examples/certbot-wildcard/certbot-wildcard.sh
@@ -3,17 +3,24 @@
 # you need to run it before starting bunkerized-nginx
 # since it's manual there is no auto renew, you need to run it again before it expires
 
+DOMAIN="*.website.com"
+SERVICE="mywww"
+
 # ask for wildcard certificate
 # it's interactive and you will need to add a DNS entry
-docker run --rm -it -v "${PWD}/letsencrypt:/etc/letsencrypt" certbot/certbot certonly --manual -d *.website.com --agree-tos --no-bootstrap
+docker run --rm -it -v "${PWD}/letsencrypt:/etc/letsencrypt" certbot/certbot certonly --manual -d $DOMAIN --agree-tos
+if [ $? -ne 0 ] ; then
+	echo "error while getting certificate for $DOMAIN"
+	exit 1
+fi
 
 # fix permissions
-chown -R 101:101 "${PWD}/letsencrypt/live"
+chown -R 101:101 "${PWD}/letsencrypt"
 
 # reload nginx if it's already running (in case of a "renew")
-if [ -z `docker-compose ps -q mywww` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q mywww)` ]; then
+if [ -z `docker-compose ps -q $SERVICE` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $SERVICE)` ]; then
 	echo "bunkerized-nginx is not running, skipping nginx reload"
 else
 	echo "bunkerized-nginx is running, sending reload order"
-	docker-compose exec mywww nginx -s reload
+	docker-compose exec $SERVICE nginx -s reload
 fi
