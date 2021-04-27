f84fd7c9a2ff9786faac4deb0eeb5baa145479d9
bunkerity <contact@bunkerity.com>
2021-04-27 16:49:45 +0200

    fix permissions issues for autoconf and fix volume for ghost example

--- entrypoint/permissions.sh
+++ entrypoint/permissions.sh
@@ -6,20 +6,24 @@ if [ ! -w "/etc/letsencrypt" ] || [ ! -r "/etc/letsencrypt" ] || [ ! -x "/etc/le
 	exit 1
 fi
 
-# /www
-if [ ! -r "/www" ] || [ ! -x "/www" ] ; then
-	echo "[!] ERROR - wrong permissions on /www"
-	exit 2
-fi
+if [ -f "/usr/sbin/nginx" ] ; then
+	# /www
+	if [ ! -r "/www" ] || [ ! -x "/www" ] ; then
+		echo "[!] ERROR - wrong permissions on /www"
+		exit 2
+	fi
 
-# /etc/nginx
-if [ ! -w "/etc/nginx" ] || [ ! -r "/etc/nginx" ] || [ ! -x "/etc/nginx" ] ; then
-	echo "[!] ERROR - wrong permissions on /etc/nginx"
-	exit 3
 fi
 
 # /acme-challenge
 if [ ! -w "/acme-challenge" ] || [ ! -r "/acme-challenge" ] || [ ! -x "/acme-challenge" ] ; then
 	echo "[!] ERROR - wrong permissions on /acme-challenge"
+	exit 3
+fi
+
+# /etc/nginx
+if [ ! -w "/etc/nginx" ] || [ ! -r "/etc/nginx" ] || [ ! -x "/etc/nginx" ] ; then
+	echo "[!] ERROR - wrong permissions on /etc/nginx"
 	exit 4
 fi
+
