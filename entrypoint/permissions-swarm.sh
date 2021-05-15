d02985d21303f3ba2ae280a2690e0cd000c19b17
bunkerity <contact@bunkerity.com>
2021-05-15 21:08:35 +0200

    check permissions for missing volumes and add comment about permissions on examples

--- entrypoint/permissions-swarm.sh
+++ entrypoint/permissions-swarm.sh
@@ -12,14 +12,36 @@ if [ ! -r "/www" ] || [ ! -x "/www" ] ; then
 	exit 2
 fi
 
+# /modsec-confs
+if [ ! -r "/modsec-confs" ] || [ ! -x "/modsec-confs" ] ; then
+	echo "[!] ERROR - wrong permissions on /modsec-confs"
+	exit 3
+fi
+# /modsec-crs-confs
+if [ ! -r "/modsec-crs-confs" ] || [ ! -x "/modsec-crs-confs" ] ; then
+	echo "[!] ERROR - wrong permissions on /modsec-crs-confs"
+	exit 4
+fi
+# /server-confs
+if [ ! -r "/server-confs" ] || [ ! -x "/server-confs" ] ; then
+	echo "[!] ERROR - wrong permissions on /server-confs"
+	exit 5
+fi
+# /http-confs
+if [ ! -r "/http-confs" ] || [ ! -x "/http-confs" ] ; then
+	echo "[!] ERROR - wrong permissions on /http-confs"
+	exit 6
+fi
+
 # /etc/nginx
 if [ ! -r "/etc/nginx" ] || [ ! -x "/etc/nginx" ] ; then
 	echo "[!] ERROR - wrong permissions on /etc/nginx"
-	exit 3
+	exit 7
 fi
 
 # /acme-challenge
 if [ ! -r "/acme-challenge" ] || [ ! -x "/acme-challenge" ] ; then
 	echo "[!] ERROR - wrong permissions on /acme-challenge"
-	exit 4
+	exit 8
 fi
+
--- entrypoint/permissions.sh
+++ entrypoint/permissions.sh
@@ -2,7 +2,7 @@
 
 # /etc/letsencrypt
 if [ ! -w "/etc/letsencrypt" ] || [ ! -r "/etc/letsencrypt" ] || [ ! -x "/etc/letsencrypt" ] ; then
-	echo "[!] WARNING - wrong permissions on /etc/letsencrypt"
+	echo "[!] ERROR - wrong permissions on /etc/letsencrypt"
 	exit 1
 fi
 
@@ -12,18 +12,36 @@ if [ -f "/usr/sbin/nginx" ] ; then
 		echo "[!] ERROR - wrong permissions on /www"
 		exit 2
 	fi
-
+	# /modsec-confs
+	if [ ! -r "/modsec-confs" ] || [ ! -x "/modsec-confs" ] ; then
+		echo "[!] ERROR - wrong permissions on /modsec-confs"
+		exit 3
+	fi
+	# /modsec-crs-confs
+	if [ ! -r "/modsec-crs-confs" ] || [ ! -x "/modsec-crs-confs" ] ; then
+		echo "[!] ERROR - wrong permissions on /modsec-crs-confs"
+		exit 4
+	fi
+	# /server-confs
+	if [ ! -r "/server-confs" ] || [ ! -x "/server-confs" ] ; then
+		echo "[!] ERROR - wrong permissions on /server-confs"
+		exit 5
+	fi
+	# /http-confs
+	if [ ! -r "/http-confs" ] || [ ! -x "/http-confs" ] ; then
+		echo "[!] ERROR - wrong permissions on /http-confs"
+		exit 6
+	fi
 fi
 
 # /acme-challenge
 if [ ! -w "/acme-challenge" ] || [ ! -r "/acme-challenge" ] || [ ! -x "/acme-challenge" ] ; then
 	echo "[!] ERROR - wrong permissions on /acme-challenge"
-	exit 3
+	exit 7
 fi
 
 # /etc/nginx
 if [ ! -w "/etc/nginx" ] || [ ! -r "/etc/nginx" ] || [ ! -x "/etc/nginx" ] ; then
 	echo "[!] ERROR - wrong permissions on /etc/nginx"
-	exit 4
+	exit 8
 fi
-
