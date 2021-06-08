177a82ee6e5abe23563794b2e534b5411e8f97d0
bunkerity <contact@bunkerity.com>
2021-06-08 09:06:21 +0200

    docs - automated sitemap.yml

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,34 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+
+def get_git_branch():
+    """Get the git branch this repository is currently on"""
+    path_to_here = os.path.abspath(os.path.dirname(__file__))
+
+    # Invoke git to get the current branch which we use to get the theme
+    try:
+        p = subprocess.Popen(['git', 'branch'], stdout=subprocess.PIPE, cwd=path_to_here)
+
+        # This will contain something like "* (HEAD detached at origin/MYBRANCH)"
+        # or something like "* MYBRANCH"
+        branch_output = p.communicate()[0]
+
+        # This is if git is in a normal branch state
+        match = re.search(r'\* (?P<branch_name>[^\(\)\n ]+)', branch_output)
+        if match:
+            return match.groupdict()['branch_name']
+
+        # git is in a detached HEAD state
+        match = re.search(r'\(HEAD detached at origin/(?P<branch_name>[^\)]+)\)', branch_output)
+        if match:
+            return match.groupdict()['branch_name']
+    except Exception:
+        print(u'Could not get the branch')
+
+    # Couldn't figure out the branch probably due to an error
+    return None
 
 # -- Project information -----------------------------------------------------
 
@@ -22,7 +50,7 @@ copyright = '2021, bunkerity'
 author = 'bunkerity'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.2.5'
+release = 'v1.2.6'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +58,7 @@ release = 'v1.2.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'sphinx_sitemap']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -53,3 +81,9 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+branch = get_git_branch()
+if branch == "master" :
+	html_baseurl = 'https://bunkerized-nginx.readthedocs.io/en/latest/'
+else :
+	html_baseurl = 'https://bunkerized-nginx.readthedocs.io/en/dev/'
