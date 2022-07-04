2b59086f6658f6030a4a99d429e87f7587f26cff
florian <contact@bunkerity.com>
2022-07-04 12:07:02 +0200

    examples - fix docker-configs

--- tests/docker.sh
+++ tests/docker.sh
@@ -59,7 +59,7 @@ single_docker_test "authelia" "60" "https://$TEST_DOMAIN1_1 authelia" "https://$
 single_docker_test "drupal" "60" "https://$TEST_DOMAIN1 drupal"
 
 # docker configs
-single_docker_test "docker-configs" "30" "https://$TEST_DOMAIN1_1/hello world" "https://$TEST_DOMAIN1_2/hello world" "https://$TEST_DOMAIN1/app1 app1" "https://$TEST_DOMAIN1/app2 app2"
+single_docker_test "docker-configs" "30" "https://$TEST_DOMAIN1_1/hello world" "https://$TEST_DOMAIN1_2/hello world" "https://$TEST_DOMAIN1_1/app1 app1" "https://$TEST_DOMAIN1_2/app2 app2"
 
 # ghost
 single_docker_test "ghost" "30" "https://$TEST_DOMAIN1 ghost"
