203397444209531acc61b5ec27c9a5c64cff6faf
bunkerity <contact@bunkerity.com>
2022-07-20 15:07:54 +0200

    tests - init cors and fix example_data path for autoconf

--- tests/AutoconfTest.py
+++ tests/AutoconfTest.py
@@ -76,7 +76,7 @@ class AutoconfTest(Test) :
             super()._setup_test()
             test = "/tmp/tests/" + self._name
             compose = "/tmp/tests/" + self._name + "/autoconf.yml"
-            example_data = "./examples/" + self._name + "/bw-data"
+            example_data = "/tmp/tests/" + self._name + "/bw-data"
             Test.replace_in_file(compose, r"bunkerity/bunkerweb:.*$", "10.20.1.1:5000/bw-tests:latest")
             Test.replace_in_file(compose, r"\./bw\-data:/", "/tmp/bw-data:/")
             Test.replace_in_file(compose, r"\- bw_data:/", "- /tmp/bw-data:/")
