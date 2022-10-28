e00b3fe7e0d42bdc8cae977baf8646ab4264ec4a
florian <contact@bunkerity.com>
2022-10-28 17:32:08 +0200

    ci/cd - fix wrong image name in security jobs (again)

--- tests/DockerTest.py
+++ tests/DockerTest.py
@@ -36,7 +36,7 @@ class DockerTest(Test) :
             test = "/tmp/tests/" + self._name
             compose = "/tmp/tests/" + self._name + "/docker-compose.yml"
             example_data = "/tmp/tests/" + self._name + "/bw-data"
-            Test.replace_in_file(compose, r"bunkerity/bunkerweb:.*$", "10.20.1.1:5000/bw-tests:latest")
+            Test.replace_in_file(compose, r"bunkerity/bunkerweb:.*$", "bw-tests:latest")
             Test.replace_in_file(compose, r"\./bw\-data:/", "/tmp/bw-data:/")
             Test.replace_in_file(compose, r"\- bw_data:/", "- /tmp/bw-data:/")
             for ex_domain, test_domain in self._domains.items() :
