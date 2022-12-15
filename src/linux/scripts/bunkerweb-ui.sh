c220e5997ccf6dee6fadec97a7b7fc553c7c96fd
AxyFr <a.fresquet@intech-sud.fr>
2022-12-15 09:36:41 +0100

    Linux UI fix

--- src/linux/scripts/bunkerweb-ui.sh
+++ src/linux/scripts/bunkerweb-ui.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
+export PYTHONPATH=/usr/share/bunkerweb/deps/python
+
 # function to start the UI
-start_ui() {
-  export PYTHONPATH=/usr/share/bunkerweb/deps/python/
+start() {
   echo "Starting UI"
-  set -a
-  . /etc/bunkerweb/ui.env
-  set +a
-  #export FLASK_APP=/usr/share/bunkerweb/ui/main.py
-  #python3 -m flask run --host=127.0.0.1 --port=7000
-  python3 -m gunicorn --bind=0.0.0.0:7000 --workers=1 --threads=2 --user ui --group ui main:app
+  python3 -m gunicorn --bind=0.0.0.0:7000 --chdir /usr/share/bunkerweb/ui/ --workers=1 --threads=2 --user scheduler --group scheduler main:app
 }
 
 # function to stop the UI
-stop_ui(){
+stop(){
     echo "Stoping ui service ..."
     # Check if pid file exist and remove it if so
     PID_FILE_PATH="/var/tmp/bunkerweb/ui.pid"
@@ -28,7 +24,7 @@ stop_ui(){
 }
 
 # function reload the UI
-reload_ui() {
+reload() {
   stop_ui
   # Wait for ui to stop
   sleep 5
@@ -37,16 +33,16 @@ reload_ui() {
   exit 0
 }
 
-case "$1" in
-    start)
-        start_ui
-        ;;
-    stop)
-        stop_ui
-        ;;
-    reload)
-        reload_ui
-        ;;
+case $1 in
+    "start") 
+    start
+    ;;
+    "stop") 
+    stop
+    ;;
+    "reload") 
+    reload
+    ;;
     *)
         echo "Usage: ./bunkerweb-ui.sh start|stop|reload"
         ;;
