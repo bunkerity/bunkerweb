f93dd34f6704bbd4fad1d6e6f309d087aa391058
Théophile Diot <theophile.diot900@gmail.com>
2023-06-06 09:21:28 -0400

    Extend KEEP_UPSTREAM_HEADERS setting to clientcache and reverseproxy core plugins

--- src/common/core/headers/headers.lua
+++ src/common/core/headers/headers.lua
@@ -1,14 +1,14 @@
-local class  = require "middleclass"
-local plugin = require "bunkerweb.plugin"
-local utils  = require "bunkerweb.utils"
+local class   = require "middleclass"
+local plugin  = require "bunkerweb.plugin"
+local utils   = require "bunkerweb.utils"
 
-local headers   = class("headers", plugin)
+local headers = class("headers", plugin)
 
 function headers:initialize()
-	-- Call parent initialize
-	plugin.initialize(self, "headers")
-	self.all_headers = {
-		["STRICT_TRANSPORT_SECURITY"] = "Strict-Transport-Security",
+  -- Call parent initialize
+  plugin.initialize(self, "headers")
+  self.all_headers = {
+    ["STRICT_TRANSPORT_SECURITY"] = "Strict-Transport-Security",
     ["CONTENT_SECURITY_POLICY"] = "Content-Security-Policy",
     ["REFERRER_POLICY"] = "Referrer-Policy",
     ["PERMISSIONS_POLICY"] = "Permissions-Policy",
@@ -16,24 +16,25 @@ function headers:initialize()
     ["X_FRAME_OPTIONS"] = "X-Frame-Options",
     ["X_CONTENT_TYPE_OPTIONS"] = "X-Content-Type-Options",
     ["X_XSS_PROTECTION"] = "X-XSS-Protection"
-	}
+  }
 end
 
 function headers:header()
-	-- Override upstream headers if needed
-  local ssl = utils.get_variable("AUTO_LETS_ENCRYPT") == "yes" or utils.get_variable("USE_CUSTOM_SSL") == "yes" or utils.get_variable("GENERATE_SELF_SIGNED_SSL") == "yes"
-	for variable, header in pairs(self.all_headers) do
+  -- Override upstream headers if needed
+  local ssl = utils.get_variable("AUTO_LETS_ENCRYPT") == "yes" or utils.get_variable("USE_CUSTOM_SSL") == "yes" or
+      utils.get_variable("GENERATE_SELF_SIGNED_SSL") == "yes"
+  for variable, header in pairs(self.all_headers) do
     if ngx.header[header] == nil or self.variables[variable] and self.variables["KEEP_UPSTREAM_HEADERS"] ~= "*" and utils.regex_match(self.variables["KEEP_UPSTREAM_HEADERS"], "(^| )" .. header .. "($| )") == nil then
       if header ~= "Strict-Transport-Security" or ssl then
         ngx.header[header] = self.variables[variable]
       end
     end
-	end
+  end
   -- Get variables
-	local variables, err = utils.get_multiple_variables({ "CUSTOM_HEADER" })
+  local variables, err = utils.get_multiple_variables({ "CUSTOM_HEADER" })
   if variables == nil then
-		return self:ret(false, err)
-	end
+    return self:ret(false, err)
+  end
   -- Add custom headers
   for srv, vars in pairs(variables) do
     if srv == ngx.ctx.bw.server_name then
@@ -51,21 +52,21 @@ function headers:header()
   if self.variables["REMOVE_HEADERS"] ~= "" then
     local iterator, err = ngx.re.gmatch(self.variables["REMOVE_HEADERS"], "([\\w-]+)")
     if not iterator then
-        return self:ret(false, "Error while matching remove headers: " .. err)
+      return self:ret(false, "Error while matching remove headers: " .. err)
     end
     while true do
       local m, err = iterator()
       if err then
-          return self:ret(false, "Error while matching remove headers: " .. err)
+        return self:ret(false, "Error while matching remove headers: " .. err)
       end
       if not m then
-          -- No more remove headers
-          break
+        -- No more remove headers
+        break
       end
       ngx.header[m[1]] = nil
     end
   end
-	return self:ret(true, "Edited headers for request")
+  return self:ret(true, "Edited headers for request")
 end
 
 return headers
