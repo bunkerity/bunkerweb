7a524b43e64acd4ce80bedc84d9ac4078c0b2acf
Théophile Diot <theophile.diot900@gmail.com>
2023-06-21 15:33:49 -0400

    Revert back to 30 seconds of sleep in tests ui after creating a custom config

--- tests/ui/main.py
+++ tests/ui/main.py
@@ -812,7 +812,7 @@ location /hello {
 
         assert_alert_message(driver, "was successfully created")
 
-        sleep(60)
+        sleep(30)
 
         driver.execute_script("window.open('http://www.example.com/hello','_blank');")
         driver.switch_to.window(driver.window_handles[1])
