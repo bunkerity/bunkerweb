8ae97d922dfdbe19fdfd777a8044e57d3aaf513d
Théophile Diot <tdiot@bunkerity.com>
2023-10-31 15:09:42 +0000

    Move mmdb jobs into misc and delete jobs plugin

--- src/common/core/misc/jobs/mmdb-country.py
+++ src/common/core/misc/jobs/mmdb-country.py
@@ -30,21 +30,21 @@ from logger import setup_logger  # type: ignore
 
 from jobs import cache_file, cache_hash, file_hash, is_cached_file
 
-LOGGER = setup_logger("JOBS.mmdb-asn", getenv("LOG_LEVEL", "INFO"))
-CORE_API = API(getenv("API_ADDR", ""), "job-mmdb-asn")
+LOGGER = setup_logger("JOBS.mmdb-country", getenv("LOG_LEVEL", "INFO"))
+CORE_API = API(getenv("API_ADDR", ""), "job-mmdb-country")
 CORE_TOKEN = getenv("CORE_TOKEN", None)
 status = 0
 lock = Lock()
 
 try:
     # Don't go further if the cache is fresh
-    in_cache, is_cached = is_cached_file("asn.mmdb", "month", CORE_API, CORE_TOKEN)
+    in_cache, is_cached = is_cached_file("country.mmdb", "month", CORE_API, CORE_TOKEN)
     if is_cached:
-        LOGGER.info("asn.mmdb is already in cache, skipping cache update...")
+        LOGGER.info("country.mmdb is already in cache, skipping cache update...")
         _exit(0)
 
     dl_mmdb = True
-    tmp_path = Path(sep, "var", "tmp", "bunkerweb", "asn.mmdb")
+    tmp_path = Path(sep, "var", "tmp", "bunkerweb", "country.mmdb")
     new_hash = None
 
     # Don't go further if the cache match the latest version
@@ -52,13 +52,13 @@ try:
         with lock:
             response = None
             try:
-                response = get("https://db-ip.com/db/download/ip-to-asn-lite", timeout=5)
+                response = get("https://db-ip.com/db/download/ip-to-country-lite", timeout=5)
             except RequestException:
-                LOGGER.warning("Unable to check if asn.mmdb is the latest version")
+                LOGGER.warning("Unable to check if country.mmdb is the latest version")
 
         if response and response.status_code == 200:
             _sha1 = sha1()
-            with open(str(tmp_path), "rb") as f:
+            with tmp_path.open("rb") as f:
                 while True:
                     data = f.read(1024)
                     if not data:
@@ -66,14 +66,14 @@ try:
                     _sha1.update(data)
 
             if response.content.decode().find(_sha1.hexdigest()) != -1:
-                LOGGER.info("asn.mmdb is already the latest version, skipping download...")
+                LOGGER.info("country.mmdb is already the latest version, skipping download...")
                 dl_mmdb = False
         else:
-            LOGGER.warning("Unable to check if asn.mmdb is the latest version, downloading it anyway...")
+            LOGGER.warning("Unable to check if country.mmdb is the latest version, downloading it anyway...")
 
     if dl_mmdb:
         # Compute the mmdb URL
-        mmdb_url = f"https://download.db-ip.com/free/dbip-asn-lite-{date.today().strftime('%Y-%m')}.mmdb.gz"
+        mmdb_url = f"https://download.db-ip.com/free/dbip-country-lite-{date.today().strftime('%Y-%m')}.mmdb.gz"
 
         # Download the mmdb file and save it to tmp
         LOGGER.info(f"Downloading mmdb file from url {mmdb_url} ...")
@@ -101,7 +101,7 @@ try:
     if in_cache:
         # Check if file has changed
         new_hash = file_hash(tmp_path)
-        old_hash = cache_hash("asn.mmdb", CORE_API, CORE_TOKEN)
+        old_hash = cache_hash("country.mmdb", CORE_API, CORE_TOKEN)
         if new_hash == old_hash:
             LOGGER.info("New file is identical to cache file, reload is not needed")
             _exit(0)
@@ -113,7 +113,7 @@ try:
 
     # Move it to cache folder
     LOGGER.info("Moving mmdb file to cache ...")
-    cached, err = cache_file("asn.mmdb", tmp_path, CORE_API, CORE_TOKEN, checksum=new_hash)
+    cached, err = cache_file("country.mmdb", tmp_path, CORE_API, CORE_TOKEN, checksum=new_hash)
     if not cached:
         LOGGER.error(f"Error while caching mmdb file : {err}")
         _exit(2)
@@ -125,6 +125,6 @@ try:
     status = 1
 except:
     status = 2
-    LOGGER.error(f"Exception while running mmdb-asn.py :\n{format_exc()}")
+    LOGGER.error(f"Exception while running mmdb-country.py :\n{format_exc()}")
 
 sys_exit(status)
