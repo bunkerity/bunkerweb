eeaa39a2e84e72eae6cf9c8e5e1410b5ac2ce76a
Jordan Blasenhauer <blasenhauer.jordan.dev@gmail.com>
2023-11-06 11:46:14 +0100

    update UI server
    *handle 503 with retry
    *check core status to run UI
    *add logs for running UI steps
    *add health

--- src/ui/main.py
+++ src/ui/main.py
@@ -3,20 +3,66 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi import FastAPI, Request
-from utils import exception_res
+from utils import exception_res, check_core
 from config import app_name, description, summary, version, contact, license_info, openapi_tags
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
 from dotenv import load_dotenv
 from routers import instances, plugins, config, misc, jobs, custom_configs, actions
+from pathlib import Path
+from logging import Logger
+from os.path import join, sep
+from sys import path as sys_path
+import time
 
 load_dotenv()
 MODE = os.getenv("MODE")
 
+HEALTHY_PATH = Path(sep, "var", "tmp", "bunkerweb", "ui.healthy")
+
+deps_path = join(sep, "usr", "share", "bunkerweb", "utils")
+if deps_path not in sys_path:
+  sys_path.append(deps_path)
+
+from logger import setup_logger  # type: ignore
+
+LOGGER: Logger = setup_logger("UI")
+
+from os import environ
+from .ui import UiConfig
+
+UI_CONFIG = UiConfig("ui", **environ)
+
+# Validate data to run app
+if not isinstance(UI_CONFIG.WAIT_RETRY_INTERVAL, int) and (not UI_CONFIG.WAIT_RETRY_INTERVAL.isdigit() or int(UI_CONFIG.WAIT_RETRY_INTERVAL) < 1):
+    LOGGER.error(f"Invalid WAIT_RETRY_INTERVAL provided: {UI_CONFIG.WAIT_RETRY_INTERVAL}, It must be a positive integer.")
+    exit(1)
+if not isinstance(UI_CONFIG.MAX_WAIT_RETRIES, int) and (not UI_CONFIG.MAX_WAIT_RETRIES.isdigit() or int(UI_CONFIG.MAX_WAIT_RETRIES) < 1):
+    LOGGER.error(f"Invalid MAX_WAIT_RETRIES provided: {UI_CONFIG.MAX_WAIT_RETRIES}, It must be a positive integer.")
+    exit(1)
+
+# Check CORE to run UI
+core_running = False
+
+for x in range(UI_CONFIG.MAX_WAIT_RETRIES):
+    if core_running :
+        break
+    
+    core_running = check_core()
+    time.sleep(UI_CONFIG.WAIT_RETRY_INTERVAL)
+
+if(core_running == True):
+    LOGGER.info("Communication with CORE succeed")
+
+if(core_running == False):
+    LOGGER.error("Communication with CORE failed, didn't start UI.")
+    exit(1)
+
+# Start UI app
 base = os.path.dirname(os.path.abspath(__file__))
 app = FastAPI(title=app_name, description=description, summary=summary, version=version, contact=contact, license_info=license_info, openapi_tags=openapi_tags)
-
+LOGGER.info("Starting UI")
 
 if MODE == "dev":
     app.add_middleware(
@@ -44,13 +90,28 @@ async def http_exception_handler(request, exc):
 async def validation_exception_handler(request, exc):
     return JSONResponse(exception_res(400, request.url.path, "Invalid data send on request"))
 
+try :
+    app.include_router(misc.router)
+    app.include_router(instances.router)
+    app.include_router(plugins.router)
+    app.include_router(config.router)
+    app.include_router(custom_configs.router)
+    app.include_router(jobs.router)
+    app.include_router(actions.router)
+    LOGGER.info("Adding UI API routes")
+except:
+    LOGGER.error("Adding UI API routes")
+    exit(1)
 
-app.include_router(misc.router)
-app.include_router(instances.router)
-app.include_router(plugins.router)
-app.include_router(config.router)
-app.include_router(custom_configs.router)
-app.include_router(jobs.router)
-app.include_router(actions.router)
+try :
+    app.mount("/", StaticFiles(directory=f"{base}/static"), name="static")
+    LOGGER.info("Adding static files")
+except :
+    LOGGER.error("Adding static files")
+    exit(1)
 
-app.mount("/", StaticFiles(directory=f"{base}/static"), name="static")
+# App is running
+if not HEALTHY_PATH.exists():
+    HEALTHY_PATH.write_text("ok", encoding="utf-8")
+
+LOGGER.info("UI started properly")
--- src/ui/utils.py
+++ src/ui/utils.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 import requests, traceback, json  # noqa: E401
 from starlette.exceptions import HTTPException as StarletteHTTPException
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+API = os.getenv("CORE_ADDR")
 
-def get_core_format_res(path, method, data, message):
+def get_core_format_res(path, method, data, message, retry = 1):
+    # Retry limit
+    if retry == 5 :
+            raise StarletteHTTPException(status_code=500, detail="Max retry to core for same request exceeded")
+
     req = None
-    # Request core api and store response
-    try:
-        if method.upper() == "GET":
-            req = requests.get(path)
-
-        if method.upper() == "POST":
-            req = requests.post(path, data=data)
-
-        if method.upper() == "DELETE":
-            req = requests.delete(path, data=data)
-
-        if method.upper() == "PATCH":
-            req = requests.patch(path, data=data)
-
-        if method.upper() == "PUT":
-            req = requests.put(path, data=data)
-    # Case no response from core
+    # Try request core
+    try :       
+        req = req_core(path, method)
+        # Case 503, retry request 
+        if req.status_code == 503 :
+            return get_core_format_res(path, method, data, message, retry + 1)
+        # Case error
+        if req == "error" :
+            raise StarletteHTTPException(status_code=500, detail="Impossible to connect to CORE API")
     except:
         raise StarletteHTTPException(status_code=500, detail="Impossible to connect to CORE API")
 
@@ -40,12 +40,44 @@ def get_core_format_res(path, method, data, message):
         print(req.status_code)
         print(req.status_code == requests.codes.ok)
 
-        return {"type": "success" if req.status_code in (requests.codes.ok, 201) else "error", "status": str(req.status_code), "message": message, "data": data}
+        return {"type": "success" if req.status_code.startswith('2') else "error", "status": str(req.status_code), "message": message, "data": data}
     # Case impossible to format
     except:
         print(traceback.format_exc())
-        raise StarletteHTTPException(status_code=500, detail="Impossible to proceed CORE API response")
 
+
+def req_core(path, method):
+    # Request core api and store response
+    req = None
+    try:
+        if method.upper() == "GET":
+            req = requests.get(path)
+
+        if method.upper() == "POST":
+            req = requests.post(path, data=data)
+
+        if method.upper() == "DELETE":
+            req = requests.delete(path, data=data)
+
+        if method.upper() == "PATCH":
+            req = requests.patch(path, data=data)
+
+        if method.upper() == "PUT":
+            req = requests.put(path, data=data)
+        
+        return req
+    # Case no response from core
+    except:
+        return 'error'
 
 def exception_res(status_code, path, detail):
     return {"type": "error", "status": status_code, "message": f"{path} {detail}", "data": "{}"}
+
+
+def check_core():
+    req = requests.get(f"{API}/ping")
+
+    if req.status_code.startswith("2"):
+        return True
+
+    return False
