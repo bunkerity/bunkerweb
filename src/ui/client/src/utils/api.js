ef2822bc6e9e154c77af762d592781615a8d408d
Théophile Diot <tdiot@bunkerity.com>
2023-11-23 09:46:15 +0000

    Export getCookie function in api.js

--- src/ui/client/src/utils/api.js
+++ src/ui/client/src/utils/api.js
@@ -64,7 +64,7 @@ export async function fetchAPI(
     });
 }
 
-function getCookie(name) {
+export function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
