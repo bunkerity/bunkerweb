d3672e6d711c61961be1cb87cc2aa4679b407bbb
florian <fpitance@bunkerity.com>
2023-12-04 12:01:49 +0100

    tests - show logs when haproxy fails in linux tests

--- examples/behind-reverse-proxy/setup-linux.sh
+++ examples/behind-reverse-proxy/setup-linux.sh
@@ -18,7 +18,12 @@ cp haproxy.cfg /etc/haproxy
 sed -i "s/*:8080/*:80/" /etc/haproxy/haproxy.cfg
 sed -i "s/mybunker/127.0.0.1/" /etc/haproxy/haproxy.cfg
 systemctl stop bunkerweb
+sleep 10
 systemctl stop haproxy
 systemctl start haproxy
+if [ $? -ne 0 ] ; then
+	systemctl status haproxy
+	journalctl -u haproxy.service
+fi
 
 echo "hello" > /var/www/html/index.html
