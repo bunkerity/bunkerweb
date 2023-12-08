2ca98e70c4b2be6374472eedf176756370f3c455
Jordan Blasenhauer <blasenhauer.jordan.dev@gmail.com>
2023-12-08 13:25:32 +0100

    setup now build html inline

--- src/ui/build.js
+++ src/ui/build.js
@@ -1,8 +1,8 @@
 const { execSync } = require("child_process");
 const { resolve } = require("path");
 const fs = require("fs");
-const buildDir = "static";
-// Build vite with child process
+const clientBuildDir = "static";
+// CLIENT : Build vite with child process
 function buildClient() {
   let isErr = false;
   try {
@@ -16,7 +16,7 @@ function buildClient() {
           isErr = true;
           console.log(`exec error: ${err}`);
         }
-      },
+      }
     );
   } catch (err) {
     isErr = true;
@@ -25,12 +25,12 @@ function buildClient() {
   return isErr;
 }
 
-// Change dir structure of build
+// CLIENT : Change dir structure
 function updateClientDir() {
   let isErr = false;
-  const srcDir = resolve(__dirname + `/${buildDir}/src/pages`);
-  const destDir = resolve(__dirname + `/${buildDir}/templates`);
-  const dirToRem = resolve(__dirname + `/${buildDir}/src`);
+  const srcDir = resolve(__dirname + `/${clientBuildDir}/src/pages`);
+  const destDir = resolve(__dirname + `/${clientBuildDir}/templates`);
+  const dirToRem = resolve(__dirname + `/${clientBuildDir}/src`);
 
   try {
     // Change dir position for html
@@ -42,20 +42,20 @@ function updateClientDir() {
     fs.rmSync(dirToRem, { recursive: true, force: true });
     // Change templates/page/index.html by templates/{page_name}.html
     // And move from static to templates
-    const templateDir = resolve(__dirname + `/${buildDir}/templates`);
+    const templateDir = resolve(__dirname + `/${clientBuildDir}/templates`);
 
     fs.readdir(templateDir, (err, subdirs) => {
       subdirs.forEach((subdir) => {
         // Get absolute path of current subdir
         const currPath = resolve(
-          __dirname + `/${buildDir}/templates/${subdir}`,
+          __dirname + `/${clientBuildDir}/templates/${subdir}`
         );
         // Rename index.html by subdir name
         fs.renameSync(`${currPath}/index.html`, `${currPath}/${subdir}.html`);
         // Copy file to move it from /template/page to /template
         fs.copyFileSync(
           `${currPath}/${subdir}.html`,
-          resolve(__dirname + `/static/${subdir}.html`),
+          resolve(__dirname + `/static/${subdir}.html`)
         );
       });
       fs.rmSync(templateDir, { recursive: true, force: true });
@@ -67,11 +67,34 @@ function updateClientDir() {
   return isErr;
 }
 
+// Build and update dir of setup inline html
+function buildSetup() {
+  let isErr = false;
+  try {
+    execSync(
+      "npm run build",
+      { cwd: resolve(__dirname + "/client") },
+      function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        if (err !== null) {
+          isErr = true;
+          console.log(`exec error: ${err}`);
+        }
+      }
+    );
+  } catch (err) {
+    isErr = true;
+  }
+
+  return isErr;
+}
+
 const isBuildErr = buildClient();
 if (isBuildErr)
   return console.log("Error while building client. Impossible to continue.");
 const isUpdateDirErr = updateClientDir();
 if (isUpdateDirErr)
   return console.log(
-    "Error while changing dir structure. Impossible to continue.",
+    "Error while changing dir structure. Impossible to continue."
   );
--- src/ui/setup/src/setup.js
+++ src/ui/setup/src/setup.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import Setup from "./Setup.vue";
 import { getI18n } from "@utils/lang.js";
+import "@public/css/style.css";
+import "@public/css/flag-icons.min.css";
 
 createApp(Setup)
   .use(getI18n(["setup"]))
