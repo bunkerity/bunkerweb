a55e2699b114f047516843110e4ca6b9f1c8e96c
Théophile Diot <tdiot@bunkerity.com>
2024-01-04 16:54:10 +0000

    Update gunicorn worker class to gthread

--- src/ui/gunicorn.conf.py
+++ src/ui/gunicorn.conf.py
@@ -1,16 +1,10 @@
-from os import cpu_count, getenv, sep
+from os import sep
 from os.path import join
 from sys import path as sys_path
 
 deps_path = join(sep, "usr", "share", "bunkerweb", "deps", "python")
 if deps_path not in sys_path:
     sys_path.append(deps_path)
-
-from gevent import monkey
-
-monkey.patch_all()
-
-MAX_WORKERS = int(getenv("MAX_WORKERS", max((cpu_count() or 1) - 1, 1)))
 
 wsgi_app = "main:app"
 accesslog = "/var/log/bunkerweb/ui-access.log"
@@ -22,8 +16,6 @@ reuse_port = True
 pidfile = join(sep, "var", "run", "bunkerweb", "ui.pid")
 worker_tmp_dir = join(sep, "dev", "shm")
 tmp_upload_dir = join(sep, "var", "tmp", "bunkerweb", "ui")
-workers = MAX_WORKERS
-worker_class = "gevent"
-threads = int(getenv("MAX_THREADS", MAX_WORKERS * 2))
+worker_class = "gthread"
 graceful_timeout = 0
 secure_scheme_headers = {}
