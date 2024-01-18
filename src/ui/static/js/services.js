ceb81603dd018e39bc2f8d5da7cf807c3aa781d6
Jordan Blasenhauer <blasenhauer.jordan.dev@gmail.com>
2024-01-18 14:38:14 +0100

    add clone service + precommit
    *fix a middle screen service style margin issue
    *add possibility to clone existing service
    *add precommit

--- src/ui/static/js/services.js
+++ src/ui/static/js/services.js
@@ -19,10 +19,10 @@ class ServiceModal {
     //modal forms
     this.formNewEdit = this.modal.querySelector("[data-services-modal-form]");
     this.formDelete = this.modal.querySelector(
-      "[data-services-modal-form-delete]",
+      "[data-services-modal-form-delete]"
     );
     this.submitBtn = document.querySelector(
-      "button[data-services-modal-submit]",
+      "button[data-services-modal-submit]"
     );
     //container
     this.container = document.querySelector("main");
@@ -83,6 +83,37 @@ class ServiceModal {
           this.openModal();
         }
       } catch (err) {}
+      // clone action
+      try {
+        if (
+          e.target.closest("button").getAttribute("data-services-action") ===
+          "clone"
+        ) {
+          //set form info and right form
+          const [action, serviceName] = this.getActionAndServName(e.target);
+          this.setForm(action, serviceName, serviceName, this.formNewEdit);
+          //set default value with method default
+          //get service data and parse it
+          //multiple type logic is launch at same time on relate class
+          const servicesSettings = e.target
+            .closest("[data-services-service]")
+            .querySelector("[data-services-settings]")
+            .getAttribute("data-value");
+          const obj = JSON.parse(servicesSettings);
+          this.updateModalData(obj, true);
+          // server name is unset
+          const inpServName = document.querySelector("input#SERVER_NAME");
+          inpServName.getAttribute("value", "");
+          inpServName.removeAttribute("disabled", "");
+          inpServName.value = "";
+          // clone is UI creation, so no setting should be disabled
+
+          //show modal
+          this.resetFilterInp();
+          this.changeSubmitBtn("CREATE", "valid-btn");
+          this.openModal(); //server name is unset
+        }
+      } catch (err) {}
       //new action
       try {
         if (
@@ -134,7 +165,7 @@ class ServiceModal {
       "delete-btn",
       "valid-btn",
       "edit-btn",
-      "info-btn",
+      "info-btn"
     );
     this.submitBtn.classList.add(btnType);
   }
@@ -194,15 +225,15 @@ class ServiceModal {
       //click the custom select dropdown to update select value
       select.parentElement
         .querySelector(
-          `button[data-setting-select-dropdown-btn][value='${defaultVal}']`,
+          `button[data-setting-select-dropdown-btn][value='${defaultVal}']`
         )
         .click();
 
       //set state to custom visible el
       const btnCustom = document.querySelector(
         `[data-setting-select=${select.getAttribute(
-          "data-setting-select-default",
-        )}]`,
+          "data-setting-select-default"
+        )}]`
       );
 
       this.setDisabledDefault(btnCustom, defaultMethod);
@@ -219,10 +250,11 @@ class ServiceModal {
 
   setForm(action, serviceName, oldServName, formEl) {
     this.modalTitle.textContent = `${action} ${serviceName}`;
-    formEl.setAttribute("id", `form-${action}-${serviceName}`);
+    const operation = action === "clone" ? "new" : action;
+    formEl.setAttribute("id", `form-${operation}-${serviceName}`);
     const opeInp = formEl.querySelector(`input[name="operation"]`);
-    opeInp.setAttribute("value", action);
-    opeInp.value = action;
+    opeInp.setAttribute("value", operation);
+    opeInp.value = operation;
 
     if (action === "edit" || action === "new") {
       this.showNewEditForm();
@@ -231,10 +263,18 @@ class ServiceModal {
       oldNameInp.value = oldServName;
     }
 
+    if (action === "clone") {
+      this.showNewEditForm();
+      const oldNameInp = formEl.querySelector(`input[name="OLD_SERVER_NAME"]`);
+      oldNameInp.setAttribute("value", "");
+      oldNameInp.value = "";
+    }
+
     if (action === "delete") {
       this.showDeleteForm();
-      formEl.querySelector(`[data-services-modal-text]`).textContent =
-        `Are you sure you want to delete ${serviceName} ?`;
+      formEl.querySelector(
+        `[data-services-modal-text]`
+      ).textContent = `Are you sure you want to delete ${serviceName} ?`;
       const nameInp = formEl.querySelector(`input[name="SERVER_NAME"]`);
       nameInp.setAttribute("value", serviceName);
       nameInp.value = serviceName;
@@ -286,7 +326,7 @@ class ServiceModal {
     this.modalTabsHeader.classList.remove("hidden");
   }
 
-  updateModalData(settings) {
+  updateModalData(settings, forceEnabled = false) {
     //use this to select inputEl and change value
     for (const [key, data] of Object.entries(settings)) {
       //change format to match id
@@ -344,20 +384,20 @@ class ServiceModal {
           if (inp.tagName === "SELECT") {
             inp.parentElement
               .querySelector(
-                `button[data-setting-select-dropdown-btn][value='${value}']`,
+                `button[data-setting-select-dropdown-btn][value='${value}']`
               )
               .click();
             inp.setAttribute("data-method", method);
           }
 
-          //check disabled/enabled after setting values and methods
-          this.setDisabledServ(inp, method, global);
+          if (!forceEnabled) this.setDisabledState(inp, method, global);
+          if (forceEnabled) inp.removeAttribute("disabled");
         });
       } catch (err) {}
     }
   }
 
-  setDisabledServ(inp, method, global) {
+  setDisabledState(inp, method, global) {
     if (global) return inp.removeAttribute("disabled");
 
     if (method === "ui" || method === "default") {
@@ -447,7 +487,7 @@ class Multiple {
           const attName = btn.getAttribute(`data-${this.prefix}-multiple-add`);
           //get all multiple groups
           const multipleEls = document.querySelectorAll(
-            `[data-${this.prefix}-settings-multiple*="${attName}"]`,
+            `[data-${this.prefix}-settings-multiple*="${attName}"]`
           );
           //case no schema
           if (multipleEls.length <= 0) return;
@@ -459,7 +499,7 @@ class Multiple {
           //and keep the highest num
           multipleEls.forEach((container) => {
             const ctnrName = container.getAttribute(
-              "data-services-settings-multiple",
+              "data-services-settings-multiple"
             );
             const num = this.getSuffixNumOrFalse(ctnrName);
             if (!isNaN(num) && num > topNum) topNum = num;
@@ -470,7 +510,7 @@ class Multiple {
           const setNum = +currNum === 0 ? `` : `_${currNum}`;
           //the default (schema) group is the last group
           const schema = document.querySelector(
-            `[data-${this.prefix}-settings-multiple="${attName}_SCHEMA"]`,
+            `[data-${this.prefix}-settings-multiple="${attName}_SCHEMA"]`
           );
           //clone schema to create a group with new num
           const schemaClone = schema.cloneNode(true);
@@ -508,7 +548,7 @@ class Multiple {
             .hasAttribute(`data-${this.prefix}-multiple-delete`)
         ) {
           const multContainer = e.target.closest(
-            "[data-services-settings-multiple]",
+            "[data-services-settings-multiple]"
           );
           multContainer.remove();
         }
@@ -530,13 +570,13 @@ class Multiple {
         ? name.replace(`_${splitName[splitName.length - 1]}`, "").trim()
         : name.trim();
       const relateSetting = document.querySelector(
-        `[data-setting-container=${nameSuffixLess}_SCHEMA]`,
+        `[data-setting-container=${nameSuffixLess}_SCHEMA]`
       );
       const relateCtnr = relateSetting.closest(
-        "[data-services-settings-multiple]",
+        "[data-services-settings-multiple]"
       );
       const relateCtnrName = relateCtnr.getAttribute(
-        "data-services-settings-multiple",
+        "data-services-settings-multiple"
       );
       //then we sort the setting on the right container name by suffixe number
       if (!(relateCtnrName in sortMultiples)) {
@@ -554,7 +594,7 @@ class Multiple {
   addOneMultGroup() {
     const settings = document.querySelector("[data-services-modal-form]");
     const multAddBtns = settings.querySelectorAll(
-      "[data-services-multiple-add]",
+      "[data-services-multiple-add]"
     );
     multAddBtns.forEach((btn) => {
       //check if already one (SCHEMA exclude so length >= 2)
@@ -569,7 +609,7 @@ class Multiple {
 
   showMultByAtt(att) {
     const multContainers = document.querySelectorAll(
-      `[data-services-settings-multiple^=${att}]`,
+      `[data-services-settings-multiple^=${att}]`
     );
     multContainers.forEach((container) => {
       if (
@@ -583,7 +623,7 @@ class Multiple {
 
   toggleMultByAtt(att) {
     const multContainers = document.querySelectorAll(
-      `[data-services-settings-multiple^=${att}]`,
+      `[data-services-settings-multiple^=${att}]`
     );
     multContainers.forEach((container) => {
       if (
@@ -599,7 +639,7 @@ class Multiple {
     //get schema settings
     const multiples = {};
     const schemaSettings = document.querySelectorAll(
-      `[data-setting-container$="SCHEMA"]`,
+      `[data-setting-container$="SCHEMA"]`
     );
     // loop on every schema settings
     schemaSettings.forEach((schema) => {
@@ -625,11 +665,11 @@ class Multiple {
   setMultipleToDOM(sortMultObj) {
     //we loop on each multiple that contains values to render to DOM
     for (const [schemaCtnrName, multGroupBySuffix] of Object.entries(
-      sortMultObj,
+      sortMultObj
     )) {
       //we need to access the DOM schema container
       const schemaCtnr = document.querySelector(
-        `[data-services-settings-multiple="${schemaCtnrName}"]`,
+        `[data-services-settings-multiple="${schemaCtnrName}"]`
       );
       //now we have to loop on each multiple settings group
       for (const [suffix, settings] of Object.entries(multGroupBySuffix)) {
@@ -645,14 +685,14 @@ class Multiple {
         for (const [name, data] of Object.entries(settings)) {
           //get setting container of clone container
           const settingContainer = schemaCtnrClone.querySelector(
-            `[data-setting-container="${name}"]`,
+            `[data-setting-container="${name}"]`
           );
           //replace input info and disabled state
           this.setSetting(
             data["value"],
             data["method"],
             data["global"],
-            settingContainer,
+            settingContainer
           );
         }
         //send schema clone to DOM and show it
@@ -667,7 +707,7 @@ class Multiple {
       "data-services-settings-multiple",
       schemaCtnrClone
         .getAttribute("data-services-settings-multiple")
-        .replace("_SCHEMA", suffix),
+        .replace("_SCHEMA", suffix)
     );
 
     //rename title
@@ -681,18 +721,18 @@ class Multiple {
 
     //rename setting container
     const settingCtnrs = schemaCtnrClone.querySelectorAll(
-      "[data-setting-container]",
+      "[data-setting-container]"
     );
     settingCtnrs.forEach((settingCtnr) => {
       settingCtnr.setAttribute(
         "data-setting-container",
         settingCtnr
           .getAttribute("data-setting-container")
-          .replace("_SCHEMA", suffix),
+          .replace("_SCHEMA", suffix)
       );
       settingCtnr.setAttribute(
         "id",
-        settingCtnr.getAttribute("id").replace("_SCHEMA", suffix),
+        settingCtnr.getAttribute("id").replace("_SCHEMA", suffix)
       );
     });
 
@@ -769,15 +809,15 @@ class Multiple {
       //click the custom select dropdown btn value to update select value
       select.parentElement
         .querySelector(
-          `button[data-setting-select-dropdown-btn][value='${defaultVal}']`,
+          `button[data-setting-select-dropdown-btn][value='${defaultVal}']`
         )
         .click();
 
       //set state to custom visible el
       const btnCustom = document.querySelector(
         `[data-setting-select=${select.getAttribute(
-          "data-setting-select-default",
-        )}]`,
+          "data-setting-select-default"
+        )}]`
       );
 
       this.setDisabledMultServ(btnCustom, method, global);
@@ -813,10 +853,10 @@ class Multiple {
         selects.forEach((select) => {
           const method = select.getAttribute("data-default-method");
           const name = select.getAttribute(
-            "data-services-setting-select-default",
+            "data-services-setting-select-default"
           );
           const selDOM = document.querySelector(
-            `button[data-services-setting-select='${name}']`,
+            `button[data-services-setting-select='${name}']`
           );
           if (method === "ui" || method === "default") {
             selDOM.removeAttribute("disabled", "");
@@ -851,7 +891,7 @@ class Multiple {
   hiddenIfNoMultiples() {
     //hide multiple btn if no multiple exist on a plugin
     const multiples = document.querySelectorAll(
-      `[data-${this.prefix}-settings-multiple]`,
+      `[data-${this.prefix}-settings-multiple]`
     );
     multiples.forEach((container) => {
       if (container.querySelectorAll(`[data-setting-container]`).length <= 0)
@@ -863,7 +903,7 @@ class Multiple {
 
   removePrevMultiples() {
     const multiPlugins = document.querySelectorAll(
-      `[data-${this.prefix}-settings-multiple]`,
+      `[data-${this.prefix}-settings-multiple]`
     );
     multiPlugins.forEach((multiGrp) => {
       if (
@@ -901,7 +941,7 @@ const setModal = new ServiceModal();
 const format = new FormatValue();
 const setFilterGlobal = new FilterSettings(
   "settings-filter",
-  "[data-service-content='settings']",
+  "[data-service-content='settings']"
 );
 
 const setMultiple = new Multiple("services");
