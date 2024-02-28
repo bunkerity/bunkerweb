1fe5b4a212fa748a548e1736d322c3d1e58dc54f
Jordan Blasenhauer <blasenhauer.jordan.dev@gmail.com>
2024-02-28 16:48:17 +0100

    add logs to utils

--- tests/ui/utils.py
+++ tests/ui/utils.py
@@ -171,12 +171,13 @@ def verify_select_filters(driver, page_name: str, filter_items: list):
         # Get a select filter, change value and click to get no match
         # Verify that elements are all hidden
         # If not return false, else reset and send true
-        check_result = driver.execute_script(
-            f"""const select{item['id']} = document.querySelector("[data-{page_name}-setting-select-dropdown-btn='{item["id"]}'][value='{item["value"]}']');if(!select{item['id']})""" + "{ return false };" + f"""select{item['id']}.setAttribute('value', '{item["update_value"]}');select{item['id']}.click(); const select{item['id']}Match = document.querySelectorAll("[data-{page_name}-list-item][class*='hidden']");if (select{item['id']}Match.length === 0)""" + "{ return false };"  + f"""select{item['id']}.setAttribute('value', '{item["value"]}');select{item['id']}.click();return true;"""
-        )
-
-        if not check_result:
-            log_error(f"The {item['name']} filter is not working, exiting ...")
-            exit(1)
+        set_el = driver.execute_script(f"""const select{item['id']} = document.querySelector("[data-{page_name}-setting-select-dropdown-btn='{item["id"]}'][value='{item["value"]}']');""")
+        log_info("Create variable")     
+        update_el_val = driver.execute_script(f"""if(!select{item['id']}){{ return false }};select{item['id']}.setAttribute('value', '{item["update_value"]}');select{item['id']}.click();""")
+        log_info("Update value")
+        check_el = driver.execute_script(f"""const select{item['id']}Match = document.querySelectorAll("[data-{page_name}-list-item][class*='hidden']");if (select{item['id']}Match.length === 0){{ return false }};""")
+        log_info("Check if elements are hidden")
+        reset_el = driver.execute_script(f"""select{item['id']}.setAttribute('value', '{item["value"]}');select{item['id']}.click();""")
+        log_info("Reset value")
 
         sleep(0.1)
