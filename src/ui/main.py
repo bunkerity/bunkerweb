db46580202b9b95c9df74564031ae24fa8bd0ad2
Théophile Diot <tdiot@bunkerity.com>
2024-03-05 09:20:12 +0000

    Refactor variable assignment in inject_variables function of web UI

--- src/ui/main.py
+++ src/ui/main.py
@@ -330,15 +330,17 @@ def generate_nonce():
 
 @app.context_processor
 def inject_variables():
+    metadata = db.get_metadata()
+
     # check that is value is in tuple
     return dict(
         dark_mode=app.config["DARK_MODE"],
         script_nonce=app.config["SCRIPT_NONCE"],
-        is_pro_version=db.get_metadata()["is_pro"] == "yes",
-        pro_status=db.get_metadata()["pro_status"],
-        pro_services=db.get_metadata()["pro_services"],
-        pro_expire=db.get_metadata()["pro_expire"],
-        pro_overlapped=db.get_metadata()["pro_overlapped"],
+        is_pro_version=metadata["is_pro"] == "yes",
+        pro_status=metadata["pro_status"],
+        pro_services=metadata["pro_services"],
+        pro_expire=metadata["pro_expire"],
+        pro_overlapped=metadata["pro_overlapped"],
         plugins=app.config["CONFIG"].get_plugins(),
     )
 
