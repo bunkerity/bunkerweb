e4ebb330b47630c827e5c76929f3c9cea5c6a604
Théophile Diot <tdiot@bunkerity.com>
2024-03-19 16:05:21 +0000

    Update DNS resolvers in postinstall.sh script

--- src/linux/scripts/postinstall.sh
+++ src/linux/scripts/postinstall.sh
@@ -66,10 +66,10 @@ fi
 
 # Create wizard config
 if [ "$UI_WIZARD" != "" ] ; then
-    echo -ne 'DNS_RESOLVERS=8.8.8.8 8.8.4.4\nHTTP_PORT=80\nHTTPS_PORT=443\nAPI_LISTEN_IP=127.0.0.1\nMULTISITE=yes\nUI_HOST=http://127.0.0.1:7000\nSERVER_NAME=\n' > /etc/bunkerweb/variables.env
+    echo -ne 'DNS_RESOLVERS=9.9.9.9 8.8.8.8 8.8.4.4\nHTTP_PORT=80\nHTTPS_PORT=443\nAPI_LISTEN_IP=127.0.0.1\nMULTISITE=yes\nUI_HOST=http://127.0.0.1:7000\nSERVER_NAME=\n' > /etc/bunkerweb/variables.env
     do_and_check_cmd chown nginx:nginx /etc/bunkerweb/variables.env
     do_and_check_cmd chmod 660 /etc/bunkerweb/variables.env
-    echo "" > /etc/bunkerweb/ui.env
+    touch /etc/bunkerweb/ui.env
     do_and_check_cmd chown nginx:nginx /etc/bunkerweb/ui.env
     do_and_check_cmd chmod 660 /etc/bunkerweb/ui.env
     do_and_check_cmd systemctl enable bunkerweb-ui
