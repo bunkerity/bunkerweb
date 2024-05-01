28636c0b9beff062799bb4ca896337dcf2b1918d
Théophile Diot <tdiot@bunkerity.com>
2024-05-01 14:57:14 +0200

    Update MMDB download and error handling in update-mmdb.sh

--- misc/update-mmdb.sh
+++ misc/update-mmdb.sh
@@ -13,10 +13,22 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+content_head="$(head -n 2 asn.mmdb.gz)"
+if [[ "$content_head" =~ "404 Not Found" ]]; then
+    echo "❌ The ASN database is not available for the current month for the moment."
+    exit 1
+fi
+
 curl -o country.mmdb.gz "https://download.db-ip.com/free/dbip-country-lite-$(date +%Y-%m).mmdb.gz"
 # shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
     echo "❌ Failed to download the country database."
+    exit 1
+fi
+
+content_head="$(head -n 2 country.mmdb.gz)"
+if [[ "$content_head" =~ "404 Not Found" ]]; then
+    echo "❌ The country database is not available for the current month for the moment."
     exit 1
 fi
 
