5171c5f6fb3573e47d04f20d3ee562f7a6117b55
Jordan Blasenhauer <blasenhauer.jordan.dev@gmail.com>
2024-05-18 22:58:07 +0200

    better format md
    * merge all files in one
    * order files by path order
    * remove path on title and add a more precise table of content

--- jsdoc/vue2md.js
+++ jsdoc/vue2md.js
@@ -25,7 +25,6 @@ function getDirectoriesRecursive(srcpath) {
 // Get the script part of a Vue file and create a JS file
 function vue2js() {
   const folders = getDirectoriesRecursive(inputFolder);
-  console.log(folders);
   // Read every subfolders from the input folder and get all files
   folders.forEach((folder) => {
     const files = fs.readdirSync(path.join(folder), {
@@ -61,16 +60,16 @@ function js2md() {
       ouputFolder,
       file.name
     )} -f md > ${path.join(ouputFolder, file.name.replace(".js", ".md"))}`;
-    console.log(command + "\n");
+
     // Run the command
     const { execSync } = require("child_process");
     execSync(command, (error, stdout, stderr) => {
       if (error) {
-        console.error(`exec error: ${error}`);
+        // console.error(`exec error: ${error}`);
         return;
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
+      // console.log(`stdout: ${stdout}`);
+      // console.error(`stderr: ${stderr}`);
     });
   });
   // Remove all JS files when all processes are done
@@ -94,6 +93,110 @@ function formatMd() {
     data = data.substring(index);
     fs.writeFileSync(path.join(ouputFolder, file.name), data, "utf8");
   });
+
+  // Create order using the tag title path of each file
+  const order = [];
+  files.forEach((file, id) => {
+    // Get the title from first line
+    const data = fs.readFileSync(path.join(ouputFolder, file.name), "utf8");
+    const filePath = data.split("\n")[0].replace("## ", "");
+    order.push({ path: filePath, file: file });
+  });
+
+  // Sort order by path
+  order.sort((a, b) => {
+    return a.path.localeCompare(b.path);
+  });
+
+  // Create a md file to merge
+  const merge = path.join(ouputFolder, "COMPONENTS.md");
+  fs.writeFileSync(merge, "", "utf8");
+  // Append each file in the order
+  order.forEach((item) => {
+    let data = fs.readFileSync(path.join(ouputFolder, item.file.name), "utf8");
+    fs.appendFileSync(merge, data, "utf8");
+  });
+
+  // Get data from merge
+  let data = fs.readFileSync(merge, "utf8");
+  let isLevel = true;
+  let currAttemps = 0;
+  const maxAttemps = 6;
+  while (isLevel && currAttemps < maxAttemps) {
+    currAttemps++;
+    const titles = [];
+    let tag = "#";
+    for (let i = 0; i < currAttemps; i++) {
+      tag += "#";
+    }
+    tag += " ";
+
+    console.log(tag);
+    // Each time, get the first level title and add it to the titles array
+    data.split("\n").forEach((line) => {
+      if (line.startsWith(tag) && line.includes("/")) {
+        const firstLevel = line.split("/")[0];
+        if (!titles.includes(firstLevel.replace(tag, "").trim()))
+          titles.push(firstLevel.replace(tag, ""));
+      }
+    });
+    // Create a top title at the first occurrence
+    // And remove from component the first level string
+    titles.forEach((title) => {
+      let isTitleSet = false;
+      data.split("\n").forEach((line) => {
+        // For title
+        if (line.startsWith(tag) && line.includes("/")) {
+          // Add a top title before the current line
+          if (!isTitleSet && line.includes(`${title}/`)) {
+            data = data.replace(
+              line,
+              `${tag} ${title}\n\n${line
+                .replace(tag, "#" + tag)
+                .replace(`${title}/`, "")}`
+            );
+            isTitleSet = true;
+            return;
+          }
+
+          if (line.includes(`${title}/`)) {
+            data = data.replace(
+              line,
+              line.replace(tag, "#" + tag).replace(`${title}/`, "")
+            );
+          }
+          return;
+        }
+      });
+    });
+  }
+
+  // Update the child of .vue component title
+  let componentTag = "";
+  let dataSplit = data.split("\n");
+  data.split("\n").forEach((line, id) => {
+    if (line.startsWith("#") && line.includes(".vue")) {
+      componentTag = line.split(" ")[0];
+      return;
+    }
+
+    if (
+      (line.startsWith("#") && line.includes("Parameters")) ||
+      (line.startsWith("#") && line.includes("Examples"))
+    ) {
+      const elTag = line.split(" ")[0];
+      // get line per id
+      const updateLine = line.replace(elTag, `${componentTag}#`);
+      dataSplit[id] = updateLine;
+    }
+  });
+  // Update the data adn merge
+  data = dataSplit.join("\n");
+
+  // Remove 3 first lines
+  data = data.split("\n").slice(3).join("\n");
+
+  fs.writeFileSync(merge, data, "utf8");
 }
 
 // Check that input folder exists
