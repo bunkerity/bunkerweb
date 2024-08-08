1669dab8ba0323db952c262236d437c9bf0dbe17
Théophile Diot <tdiot@bunkerity.com>
2024-08-08 13:50:53 +0100

    chore: Update entrypoint.sh script for web UI Docker container

--- src/ui/entrypoint.sh
+++ src/ui/entrypoint.sh
@@ -4,6 +4,24 @@ set -e
 # shellcheck disable=SC1091
 . /usr/share/bunkerweb/helpers/utils.sh
 
+# trap SIGTERM and SIGINT
+function trap_exit() {
+	# shellcheck disable=SC2317
+	log "ENTRYPOINT" "ℹ️ " "Caught stop operation"
+	# shellcheck disable=SC2317
+	if [ -f "/var/run/bunkerweb/ui.pid" ] ; then
+		# shellcheck disable=SC2317
+		log "ENTRYPOINT" "ℹ️ " "Stopping web UI ..."
+		# shellcheck disable=SC2317
+		kill -s TERM "$(cat /var/run/bunkerweb/ui.pid)"
+	fi
+}
+trap "trap_exit" TERM INT QUIT
+
+if [ -f /var/run/bunkerweb/ui.pid ] ; then
+	rm -f /var/run/bunkerweb/ui.pid
+fi
+
 log "ENTRYPOINT" "ℹ️" "Starting the web UI v$(cat /usr/share/bunkerweb/VERSION) ..."
 
 # setup and check /data folder
@@ -19,7 +37,16 @@ else
   echo "Docker" > /usr/share/bunkerweb/INTEGRATION
 fi
 
-python3 -m gunicorn --config gunicorn.conf.py --user ui --group ui --bind 0.0.0.0:7000
+python3 -m gunicorn --config gunicorn.conf.py --user ui --group ui --bind 0.0.0.0:7000 &
+pid="$!"
+wait "$pid"
+while [ -f /var/run/bunkerweb/ui.pid ] ; do
+		wait "$pid"
+done
+
+if [ -f /var/tmp/bunkerweb/ui.healthy ] ; then
+	rm /var/tmp/bunkerweb/ui.healthy
+fi
 
 log "ENTRYPOINT" "ℹ️" "Web UI stopped"
 exit 0
