28b7bce39fa0471f09815c327b60591fd9851e75
Théophile Diot <tdiot@bunkerity.com>
2025-02-04 09:40:22 +0100

    Fix capitalization in success flash message and tooltips for consistency

--- src/ui/app/static/js/pages/instances.js
+++ src/ui/app/static/js/pages/instances.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
   var toastNum = 0;
   var actionLock = false;
+  var showLoadingModal = false;
   const instanceNumber = parseInt($("#instances_number").val());
   const isReadOnly = $("#is-read-only").val().trim() === "True";
 
   const pingInstances = (instances) => {
+    showLoadingModal = true;
+
     setTimeout(() => {
-      if (actionLock) {
+      if (actionLock && showLoadingModal) {
         $(".dt-button-background").click();
         $("#loadingModal").modal("show");
         setTimeout(() => {
           $("#loadingModal").modal("hide");
-        }, 5300);
+        }, 5000);
       }
     }, 500);
 
@@ -58,6 +61,7 @@ $(document).ready(function () {
       },
       complete: function () {
         actionLock = false;
+        showLoadingModal = false;
         $("#loadingModal").modal("hide");
       },
     });
