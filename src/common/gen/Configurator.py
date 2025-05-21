ddd6c252b3f7676a8494c56e8b501d76286de069
Théophile Diot <tdiot@bunkerity.com>
2025-05-21 17:45:53 +0200

    feat: Add "IGNORE_REGEX_CHECK" to the list of environment variables in Configurator

--- src/common/gen/Configurator.py
+++ src/common/gen/Configurator.py
@@ -233,6 +233,7 @@ class Configurator:
                         "OLDPWD",
                         "SERVICE_SCHEDULER",
                         "SERVICE_UI",
+                        "IGNORE_REGEX_CHECK",
                         "CROWDSEC_EXTRA_COLLECTIONS",
                     )
                 )
