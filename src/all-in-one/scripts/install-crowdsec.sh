4e627ab450e16d6fe2ebb8a3db01b4d901f08cb1
TheophileDiot <tdiot@bunkerity.com>
2025-06-05 16:01:14 +0200

    feat: Temporarily remove 'all-in-one' image from ARM build matrices in workflows and update Dockerfile dependencies

--- src/all-in-one/scripts/install-crowdsec.sh
+++ src/all-in-one/scripts/install-crowdsec.sh
@@ -9,11 +9,6 @@ echo "ℹ️ Cloning and building CrowdSec $VERSION"
 echo "ℹ️ Cloning CrowdSec from $URL (commit $COMMIT)"
 git_clone_commit crowdsec "$URL" "$COMMIT"
 
-if [[ "$TARGETPLATFORM" == "linux/arm64" || "$TARGETPLATFORM" == "linux/arm/v7" ]]; then
-  export GOARCH=arm
-  export CGO=1
-fi
-
 echo "ℹ️ Building CrowdSec"
 make BUILD_VERSION="$VERSION" BUILD_STATIC=1 release
 
