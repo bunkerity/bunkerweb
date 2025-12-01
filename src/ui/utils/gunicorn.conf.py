54ded3a63eafa88855e82920281f90daee2669c3
TheophileDiot <tdiot@bunkerity.com>
2025-12-01 11:36:26 +0100

    feat(logging): Ensure syslog address defaults to UDP if not specified

--- src/ui/utils/gunicorn.conf.py
+++ src/ui/utils/gunicorn.conf.py
@@ -65,6 +65,8 @@ else:
 if "syslog" in log_types:
     syslog = True
     syslog_addr = getenv("LOG_SYSLOG_ADDRESS", "").strip()
+    if not syslog_addr.startswith(("/", "udp://", "tcp://")):
+        syslog_addr = f"udp://{syslog_addr}"
     syslog_prefix = getenv("LOG_SYSLOG_TAG", "bw-ui")
 
 wsgi_app = "main:app"
