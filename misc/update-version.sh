74147aa2b643da5acd6be2a63a4c5ba4c8c76b10
TheophileDiot <tdiot@bunkerity.com>
2026-02-23 17:37:26 +0100

    Road to 1.6.9~rc2 🚀

--- misc/update-version.sh
+++ misc/update-version.sh
@@ -86,9 +86,6 @@ sed -i "s@LABEL version.*@LABEL version=\"$NEW_VERSION_UNESCAPED\"@g" src/autoco
 # easy-install script
 sed -i "s@DEFAULT_BUNKERWEB_VERSION=.*@DEFAULT_BUNKERWEB_VERSION=\"$NEW_VERSION_UNESCAPED\"@g" misc/install-bunkerweb.sh
 # publiccode.yml
-# if non stable we don't update publiccode.yml
-if [[ ! "$1" =~ ~ ]] ; then
-    sed -i "s@softwareVersion: .*@softwareVersion: $NEW_VERSION_UNESCAPED@g" publiccode.yml
-    sed -i "s@releaseDate: .*@releaseDate: $(date +%Y-%m-%d)@g" publiccode.yml
-    sed -i "s@logo: .*@logo: https://github.com/bunkerity/bunkerweb/raw/v$NEW_VERSION_UNESCAPED/misc/logo.png@g" publiccode.yml
-fi
+sed -i "s@softwareVersion: .*@softwareVersion: $NEW_VERSION_UNESCAPED@g" publiccode.yml
+sed -i "s@releaseDate: .*@releaseDate: $(date +%Y-%m-%d)@g" publiccode.yml
+sed -i "s@logo: .*@logo: https://github.com/bunkerity/bunkerweb/raw/v$NEW_DOCKER_VERSION/misc/logo.png@g" publiccode.yml
