e6eb98147bde4285716699437df173fd61a4b116
TheophileDiot <tdiot@bunkerity.com>
2026-08-21 16:53:47 +0200

    ci(deps): bump codeql-action to v4.37.7, setup-buildx-action to v4.3.0, scaleway provider to 2.81.0

--- tests/terraform/providers.tf
+++ tests/terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     scaleway = {
       source = "scaleway/scaleway"
-      version = "2.79.0"
+      version = "2.81.0"
     }
     kubectl = {
       source = "gavinbunney/kubectl"
